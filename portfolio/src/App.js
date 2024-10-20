a63ce334f20aebedc7a847b04169e06982e7937b
jameswen <leranjamesw@gmail.com>
2024-10-20 14:38:13 -0700

    added background

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,7 +6,7 @@ import React, {useState, useEffect} from "react";
 function Introduction() {
   const [title, setTitle] = useState("");
   const [index, setIndex] = useState(0);
-  const typingSpeed = 100;
+  const typingSpeed = 60;
 
   const titleText = "👋 Hi, welcome to my personal website!"
 
@@ -22,10 +22,10 @@ function Introduction() {
 
   }, [index, titleText]);
   return (
-    <header class = "flex flex-col items-center space-x-1 font-mono">
-      <h1 class = "text-4xl">James Wen</h1>
+    <header class = "flex flex-col items-center space-x-1 font-mono text-slate-900">
+      <h1 class = "text-5xl p-6 font-bold">James Wen</h1>
       <div class = "flex flex-row">
-        <span class = "text-xl">{title}</span>
+        <span class = "text-2xl">{title}</span>
         <span class ="border-l-2 border-black animate-blink box-shadow-none text-white">|</span>
       </div>
     </header>
@@ -35,12 +35,17 @@ function Introduction() {
 
 function App() {
   return (
-    <div class="h-dvh container mx-auto items-center">
-      <div class = "container mx-auto w-2/3 h-1/8 flex rounded-lg shadow-lg">
-        <Navigation/>
+    <div class = "overflow-x-hidden">
+      <div class="h-screen w-screen container mx-auto flex flex-col justify-center items-center bg-fixed bg-cover bg-[url('/public/SF.jpg')]">
+        <div class = "container mx-auto fixed top-2 w-2/3 h-1/8 flex rounded-lg shadow-lg items-center bg-white">
+          <Navigation/>
+        </div>
+        <div class = "mt-0 container mx-auto shadow-lg w-1/2 h-2/5 flex items-center rounded-lg justify-center bg-white">
+          <Introduction/>
+        </div>
       </div>
-      <div class = "mt-6 container mx-auto shadow-lg w-1/2 h-1/3 flex items-center rounded-lg justify-center">
-        <Introduction/>
+      <div class = "p-6 container mx-auto flex justify-center items-center w-screen h-1/2">
+        <div>About me</div>
       </div>
     </div>
   );
--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -6,14 +6,14 @@ import React, {useState, useEffect} from "react";
 
 function NavigationButton() {
     return (
-        <button class = "px-4 py-1 text-sm text-blue-600 font-semibold rounded-full border border-blue-200 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2">
+        <button class = "px-4 py-1 text-sm text-slate-900 font-semibold rounded-full border border-slate-200 hover:text-white hover:bg-slate-900 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-slate-900 focus:ring-offset-2">
             button
         </button>
     )
 }
 function Navigation() {
     return (
-        <div class="flex flex-row">
+        <div class="flex flex-row p-2">
             <NavigationButton/>
             <NavigationButton/>
             <NavigationButton/>
