8d72bac88cd1501a76adf70638c30e6edf1fbbe8
jameswen <leranjamesw@gmail.com>
2024-10-25 00:46:00 -0700

    Implemented Navigation buttons

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navigation from './components/Navigation';
 import Projects from './components/Projects';
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 
 function Introduction() {
@@ -27,7 +27,7 @@ function Introduction() {
       <h1 class = "text-5xl p-6 font-bold">James Wen</h1>
       <div class = "flex flex-row">
         <span class = "text-2xl font-mono">👋 {title}</span>
-        <span class ="border-l-2 border-black animate-blink box-shadow-none text-white">|</span>
+        <span class ="lg:border-l-2 border-black animate-blink box-shadow-none text-white">&#8203;</span>
       </div>
     </header>
   );
@@ -35,38 +35,81 @@ function Introduction() {
 
 function AboutMe() {
   return(
-    <div class = "flex flex-col flex justify-center items-center">
-      <h2 class = "text-4xl p-2 font-bold text-slate-800">About Me</h2>
-      <div class = "flex flex-col p-2 w-3/4">
-        <p class = "text-lg text-slate-800">
+    <div class = "mt-8 mb-8 flex flex-col flex justify-center items-center">
+      <div></div>
+      <h2 class = "text-4xl p-4 font-bold text-slate-800">About Me</h2>
+      <div class = "flex flex-col p-2 w-3/4 font-serif">
+        <p class = "text-indent-2 text-lg text-slate-800 mb-6">
         Hi! My name is James, and I am currently a 3rd year Computer Science 
         and Statistics student at the University of British Columbia! I love 
         innovation and creativity in the field of technology and seeing all 
         the fascinating ways innovators in this field have been able to apply 
         technology in other industries for the greater good.
         </p>
+        <p class = "text-indent-2 text-lg text-slate-800 mb-2">
+        I decided to study computer science in university because of my 
+        experience as a competitive programmer in highschool. Ever since then, 
+        I've competed in programming contests, hackathons, and even game jams. 
+        My journey in web development began very recently but I've been able to
+        gain a multitude of experiences and knowledge through personal and 
+        academic full-stack projects. Most of my prior experiences in software 
+        development are on the data structures/algorithm side. 
+        I've previously interned at AUAV Technology, a drone startup, 
+        as a software engineering intern where I developed 
+        Python scripts to facilitate hand-tracking functionalities for drone 
+        control programs. These scripts leveraged AI and computer vision 
+        techniques by integrating Google’s MediaPipe API with OpenCV, enabling 
+        real-time hand gesture recognition and interaction. 
+        </p>
       </div>
     </div>
   );
 }
 
+function Contact() {
+  return(
+    <div id = "Contact">
+      <span>Email: leranjamesw@gmail.com</span>
+      <a class = "hover:underline">Github</a>
+      <a class = "hover:underline">LinkedIn</a>
+    </div>
+  )
+}
 
 function App() {
+  const aboutMeDivRef = useRef(null);
+  const projectsDivRef = useRef(null);
+  const contactDivRef = useRef(null);
+  
+  const handleScroll = (e, divRef) => {
+    if (divRef.current) {
+      divRef.current.scrollIntoView({behavior:"smooth"});
+    }
+    e.target.blur();
+  }
+
   return (
     <div class = "overflow-x-hidden">
-      <div class="h-screen w-screen container mx-auto flex flex-col justify-center items-center bg-fixed bg-cover bg-[url('/public/SF.jpg')]">
-        <div class = "container mx-auto fixed top-2 w-2/3 h-1/8 flex rounded-lg shadow-lg items-center bg-white z-10">
-          <Navigation/>
+      <div class = "h-screen min-w-full container mx-auto flex flex-col justify-center items-center bg-fixed bg-cover bg-[url('/public/SF.jpg')]">
+        <div class = "container mx-auto fixed justify-end top-2 w-2/3 h-1/8 flex rounded-2xl shadow-xl bg-slate-100 z-10">
+          <Navigation 
+            onScroll = {handleScroll}
+            aboutMeDivRef = {aboutMeDivRef}
+            projectsDivRef = {projectsDivRef}
+            contactDivRef = {contactDivRef}/>
         </div>
-        <div class = "mt-0 container mx-auto shadow-lg w-1/2 h-2/5 flex items-center rounded-lg justify-center bg-white">
+        <div class = "mt-0 container mx-auto shadow-lg w-1/2 h-2/5 flex items-center rounded-lg justify-center bg-slate-100">
           <Introduction/>
         </div>
       </div>
-      <div class = "p-6 container mx-auto w-screen">
+      <div ref = {aboutMeDivRef} class = "p-6 container mx-auto min-w-full bg-slate-100">
         <AboutMe/>
       </div>
-      <div class = "p-6 container mx-auto w-screen bg-blue-950">
+      <div ref = {projectsDivRef} class = "p-4 container mx-auto min-w-full bg-blue-950 h-1/2">
         <Projects/>
+      </div>
+      <div ref = {contactDivRef} class = "p-4 container mx-auto min-w-full bg-blue-950 h-1/2">
+
       </div>
     </div>
   );
--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -4,19 +4,41 @@ import React, {useState, useEffect} from "react";
 
 
 
-function NavigationButton() {
+function NavigationButton(props) {
+    const name = props.name
+    const onScroll = props.onScroll
+    const divRef = props.divRef
+
     return (
-        <button class = "px-4 py-1 text-sm text-slate-900 font-semibold rounded-full border border-slate-200 hover:text-white hover:bg-slate-900 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-slate-900 focus:ring-offset-2">
-            button
-        </button>
+        <div class = "ml-1 mr-1">
+            <button 
+                onClick={(e)=> onScroll(e, divRef)}
+                class = "px-4 py-1 text-medium text-slate-900 font-semibold rounded-full border-2 border-slate-200 hover:text-white hover:bg-slate-900 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-slate-900 focus:ring-offset-2">
+                {name}
+            </button>
+        </div>
     )
 }
-function Navigation() {
+
+function Navigation(props) {
+    const onScroll = props.onScroll
+    const aboutMeDivRef = props.aboutMeDivRef
+    const projectsDivRef = props.projectsDivRef
+    const contactDivRef = props.contactDivRef
     return (
-        <div class="flex flex-row p-2">
-            <NavigationButton/>
-            <NavigationButton/>
-            <NavigationButton/>
+        <div class="flex flex-row p-2 justify-end mr-2">
+            <NavigationButton 
+                name = "About Me" 
+                onScroll = {onScroll}
+                divRef = {aboutMeDivRef}/>
+            <NavigationButton 
+                name = "Projects"
+                onScroll = {onScroll}
+                divRef = {projectsDivRef}/>
+            <NavigationButton 
+                name = "Contact"
+                onScroll = {onScroll}
+                divRef = {contactDivRef}/>
         </div>
     )
 }
